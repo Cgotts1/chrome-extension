f8a4a6af33bca63ab85ca82c1ab45348f93d2136
Cgotts <cobi.gottschalk@gmail.com>
2022-11-20 18:30:38 -0800

    Added some js html elements

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let inputBtn = document.querySelector("#input-btn")
+let myLeads = ["blah", "scoop", "roop"];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el")
 
-// inputBtn.addEventListener("click", btnClicked)
 
-// function btnClicked(){
-//     console.log("Button clicked!")
-// }
-
-let btnClicked = ()=>{                   //with arrow() need to addevent listener below or it doesnt work
-  console.log("Button clicked!")
+let btnClicked = ()=>{        
+  myLeads.push(inputEl.value)  // get value from text into array
+  console.log(myLeads)
 }
+
 inputBtn.addEventListener("click", btnClicked)
+
+let listItems = " ";
+
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>"   //Turns it to li list
+}
+
+ulEl.innerHTML +=  listItems      //put it outside of the loop so it renders 1 time

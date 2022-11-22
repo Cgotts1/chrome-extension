877efec72e4324e767503782e61824b91577644d
Cgotts <cobi.gottschalk@gmail.com>
2022-11-21 18:19:46 -0800

    added more

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-let myLeads = ["blah", "scoop", "roop"];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
-const ulEl = document.querySelector("#ul-el")
+const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
-let btnClicked = ()=>{        
-  myLeads.push(inputEl.value)  // get value from text into array
-  console.log(myLeads)
+
+    if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+  }
+  
+
+  function renderLeads() {
+    let listItems = " ";
+  
+    for (let i = 0; i < myLeads.length; i++) {
+      // listItems = "<li><a target ='_blank' href = '#'>" + myLeads[i] + "</a></li>"   //Turns it to li list   chaning += to just =
+      listItems += `   
+      <li>
+       <a target ='_blank' href = "${myLeads[i]}">
+        ${myLeads[i]}
+        </a>
+       </li>
+       `; //Turns it to li list   chaning += to just =
+    }
+    ulEl.innerHTML += listItems;
+  } //put it outside of the loop so it renders 1 time
+
+
+
+
+const deleteAll = ()=>{
+  localStorage.clear();
+  myLeads = [];
+  ulEl.innerHTML = ""
 }
 
-inputBtn.addEventListener("click", btnClicked)
+deleteBtn.addEventListener("dblclick", deleteAll)
 
-let listItems = " ";
 
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"   //Turns it to li list
-}
+let btnClicked = () => {
+  myLeads.push(inputEl.value); // get value from text into array
+  console.log(myLeads);
+  inputEl.value = " "; //Clears input field
 
-ulEl.innerHTML +=  listItems      //put it outside of the loop so it renders 1 time
+localStorage.setItem("myLeads", JSON.stringify(myLeads)); // Turns stringS
+  renderLeads();
+  // console.log(localStorage.getItem("myLeads"))
+};
+
+inputBtn.addEventListener("click", btnClicked);
